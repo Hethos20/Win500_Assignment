1f94abdaf9f7b55fbb531c4a32721ff13b667342
Hethos20 <teenmonkey123@gmail.com>
2020-08-04 18:30:10 -0400

    Update assignment.ps1

--- assignment.ps1
+++ assignment.ps1
@@ -1,8 +1,13 @@
-
 #get all computers that are in domain
-$computers = Get-ADComputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' `
--Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address |
-Sort-Object -Property Operatingsystem |
-Select-Object -Property Name,Operatingsystem,OperatingSystemVersion,IPv4Address
+$ADCOMPS=@(Get-ADComputer -Filter {OperatingSystem -like "Windows Server*"}   -Properties Name | select name | ft -HideTableHeaders)
 
+$table = @()
 
+foreach ($x in $ADCOMPS){
+    #get hard drive size
+    $hdfreespace = Get-WmiObject Win32_LogicalDisk -ComputerName $x | Measure-Object -Property Freespace -Sum | % {[Math]::Round(($_.sum / 1MB),2)}
+    $hdSize = Get-WmiObject Win32_LogicalDisk -ComputerName $x | Measure-Object -Property Size -Sum | % {[Math]::Round(($_.sum / 1MB),2)}
+    # get ram size
+    $ramSize = Get-WMIObject -class Win32_PhysicalMemory -ComputerName $x | Measure-Object -Property capacity -Sum | % {[Math]::Round(($_.sum / 1GB),2)}
+    $table.Add($x,$hdfreespace,$hdSize,$ramSize)
+}
