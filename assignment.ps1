5ea659088b2118f4ce30db696dd9433fe5eff6e4
Hethos20 <teenmonkey123@gmail.com>
2020-08-04 16:27:52 -0400

    Create assignment.ps1

---
+++ assignment.ps1
@@ -0,0 +1,6 @@
+
+#
+Get-ADComputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' `
+-Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address |
+Sort-Object -Property Operatingsystem |
+Select-Object -Property Name,Operatingsystem,OperatingSystemVersion,IPv4Address
