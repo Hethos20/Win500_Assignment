e9a6b32f5c475103a1e39f25ec1df4d82cfc0859
Hethos20 <teenmonkey123@gmail.com>
2020-08-04 16:29:16 -0400

    Update assignment.ps1

--- assignment.ps1
+++ assignment.ps1
@@ -1,6 +1,8 @@
 
-#
-Get-ADComputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' `
+#get all computers that are in domain
+$computers = Get-ADComputer -Filter 'operatingsystem -like "*server*" -and enabled -eq "true"' `
 -Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address |
 Sort-Object -Property Operatingsystem |
 Select-Object -Property Name,Operatingsystem,OperatingSystemVersion,IPv4Address
+
+
