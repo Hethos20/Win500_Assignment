ed2a1373954c72fd88db27f6bf8eae544e55bd65
Hethos20 <teenmonkey123@gmail.com>
2020-08-04 19:07:38 -0400

    Update assignment.ps1

--- assignment.ps1
+++ assignment.ps1
@@ -1,13 +1,22 @@
 #get all computers that are in domain
 $ADCOMPS=@(Get-ADComputer -Filter {OperatingSystem -like "Windows Server*"}   -Properties Name | select name | ft -HideTableHeaders)
 
-$table = @()
-
 foreach ($x in $ADCOMPS){
-    #get hard drive size
-    $hdfreespace = Get-WmiObject Win32_LogicalDisk -ComputerName $x | Measure-Object -Property Freespace -Sum | % {[Math]::Round(($_.sum / 1MB),2)}
-    $hdSize = Get-WmiObject Win32_LogicalDisk -ComputerName $x | Measure-Object -Property Size -Sum | % {[Math]::Round(($_.sum / 1MB),2)}
-    # get ram size
-    $ramSize = Get-WMIObject -class Win32_PhysicalMemory -ComputerName $x | Measure-Object -Property capacity -Sum | % {[Math]::Round(($_.sum / 1GB),2)}
-    $table.Add($x,$hdfreespace,$hdSize,$ramSize)
+    
+    # if computer is online
+    if (!(Test-Connection $x -Count 1)){
+        Write-Warning "Computer is not active"
+        break
+    }
+
+    else{
+        $customObj = New-Object psobject -Property @{`
+            "Computer" = Invoke-Command -ComputerName $x -ScriptBlock {$env:computername};
+            "HDD_Freespace" = Get-WmiObject Win32_LogicalDisk -ComputerName $x | Measure-Object -Property Freespace -Sum | % {[Math]::Round(($_.sum / 1MB),2)};
+            "HDD_Size" = Get-WmiObject Win32_LogicalDisk -ComputerName $x | Measure-Object -Property Size -Sum | % {[Math]::Round(($_.sum / 1MB),2)};
+            "Ram_Size" = Get-WMIObject -class Win32_PhysicalMemory -ComputerName $x | Measure-Object -Property capacity -Sum | % {[Math]::Round(($_.sum / 1GB),2)}
+        }
+        
+        $returnObj += $customObj | select HDD_Freespace,HDD_Size,Ram_Size
+    }
 }
