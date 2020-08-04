2488359f6e2c54e2b7ba80afeffe0969dbae5412
Hethos20 <teenmonkey123@gmail.com>
2020-08-04 19:14:45 -0400

    Update assignment.ps1

--- assignment.ps1
+++ assignment.ps1
@@ -1,22 +1,29 @@
-#get all computers that are in domain
-$ADCOMPS=@(Get-ADComputer -Filter {OperatingSystem -like "Windows Server*"}   -Properties Name | select name | ft -HideTableHeaders)
-
-foreach ($x in $ADCOMPS){
+function comp_info{
+    param(
+        $compName
+    )
     
     # if computer is online
-    if (!(Test-Connection $x -Count 1)){
+    if (!(Test-Connection $compName -Count 1)){
         Write-Warning "Computer is not active"
         break
     }
 
     else{
         $customObj = New-Object psobject -Property @{`
-            "Computer" = Invoke-Command -ComputerName $x -ScriptBlock {$env:computername};
-            "HDD_Freespace" = Get-WmiObject Win32_LogicalDisk -ComputerName $x | Measure-Object -Property Freespace -Sum | % {[Math]::Round(($_.sum / 1MB),2)};
-            "HDD_Size" = Get-WmiObject Win32_LogicalDisk -ComputerName $x | Measure-Object -Property Size -Sum | % {[Math]::Round(($_.sum / 1MB),2)};
-            "Ram_Size" = Get-WMIObject -class Win32_PhysicalMemory -ComputerName $x | Measure-Object -Property capacity -Sum | % {[Math]::Round(($_.sum / 1GB),2)}
+            "Computer" = Invoke-Command -ComputerName $compName -ScriptBlock {$env:computername};
+            "HDD_Freespace" = Get-WmiObject Win32_LogicalDisk -ComputerName $compName | Measure-Object -Property Freespace -Sum | % {[Math]::Round(($_.sum / 1MB),2)};
+            "HDD_Size" = Get-WmiObject Win32_LogicalDisk -ComputerName $compName | Measure-Object -Property Size -Sum | % {[Math]::Round(($_.sum / 1MB),2)};
+            "Ram_Size" = Get-WMIObject -class Win32_PhysicalMemory -ComputerName $compName | Measure-Object -Property capacity -Sum | % {[Math]::Round(($_.sum / 1GB),2)}
         }
         
         $returnObj += $customObj | select HDD_Freespace,HDD_Size,Ram_Size
     }
+}
+
+#get all computers that are in domain
+$ADCOMPS=@(Get-ADComputer -Filter {OperatingSystem -like "Windows Server*"}   -Properties Name | select name | ft -HideTableHeaders)
+
+foreach ($comp in $ADCOMPS){
+    comp_info -compName $comp
 }
